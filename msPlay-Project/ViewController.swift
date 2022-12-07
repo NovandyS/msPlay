4a606b4159548368096da5ce0bafee59a1c1cda4
Andy <andinovandi14@gmail.com>
2022-12-07 16:33:05 +0700

    Initial Commit

---
+++ msPlay-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  msPlay-Project
+//
+//  Created by Andy Salim on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
