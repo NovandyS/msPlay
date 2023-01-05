633b506aa6f97ba8e3cdd994adb2f848c981febd
Andy <andinovandi14@gmail.com>
2023-01-05 23:27:34 +0700

    Adding UI to Storyboard

---
+++ msPlay-Project/BrowseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BrowseViewController.swift
+//  msPlay-Project
+//
+//  Created by Andy Salim on 28/12/22.
+//
+
+import UIKit
+
+class BrowseViewController: UIViewController {
+
+    @IBOutlet weak var genreCollectionView: UICollectionView!
+    @IBOutlet weak var musicCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ msPlay-Project/HomeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  msPlay-Project
+//
+//  Created by Andy Salim on 07/12/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var headerWelcome: UIImageView!
+    
+    @IBOutlet weak var recentPlayImg1: UIImageView!
+    @IBOutlet weak var recentPlayTitle1: UILabel!
+    @IBOutlet weak var recentPlayImg2: UIImageView!
+    @IBOutlet weak var recentPlayTitle2: UILabel!
+    @IBOutlet weak var recentPlayImg3: UIImageView!
+    @IBOutlet weak var recentPlayTitle3: UILabel!
+    
+    @IBOutlet weak var topSongImg1: UIImageView!
+    @IBOutlet weak var topSongTitle1: UILabel!
+    @IBOutlet weak var topSongImg2: UIImageView!
+    @IBOutlet weak var topSongTitle2: UILabel!
+    @IBOutlet weak var topSongImg3: UIImageView!
+    @IBOutlet weak var topSongTitle3: UILabel!
+    
+    @IBOutlet weak var topPlaylistImg1: UIImageView!
+    @IBOutlet weak var topPlaylistTitle1: UILabel!
+    @IBOutlet weak var topPlaylistImg2: UIImageView!
+    @IBOutlet weak var topPlaylistTitle2: UILabel!
+    @IBOutlet weak var topPlaylistImg3: UIImageView!
+    @IBOutlet weak var topPlaylistTitle3: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
